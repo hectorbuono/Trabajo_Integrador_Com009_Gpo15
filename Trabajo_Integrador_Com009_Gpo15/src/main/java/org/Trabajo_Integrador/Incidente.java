020845071e95a0e4dd316ad6ccea73e7743ff75e
hectorbuono <hectorbuono@gmail.com>
2023-12-03 17:04:10 -0300

    importacion de las librerias javax e hibenate

--- Trabajo_Integrador_Com009_Gpo15/src/main/java/org/Trabajo_Integrador/Incidente.java
+++
@@ -1,8 +0,0 @@
-package org.Trabajo_Integrador;
-
-import javax.swing.*;
-
-public class Incidente {
-    private JButton button1;
-    private JPanel panel1;
-}
---
+++ Trabajo_Integrador_Com009_Gpo15/src/main/java/org/Trabajo_Integrador/incidentes.java
@@ -0,0 +1,31 @@
+package org.Trabajo_Integrador;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "incidentes")
+public class incidentes {
+    @Id
+    @Column(name = "id_incidente", nullable = false, length= 11)
+    private BigInteger id;
+
+    @Column(name = "nro_incidente", nullable = false, length= 11)
+    private BigInteger nro_incidente;
+
+
+    @Column(name = "tipo_incidente", nullable = false, length = 3)
+    private Integer tipo_incidente;
+
+    @Column(name = "tipo_incidente", nullable = false, length = 3)
+    private Integer tipo_incidente;
+
+}
